e6e322ee32a27f6078363e37ffe6bd232c7e3bf6
Drake Leung <drakeleung42@gmail.com>
2016-06-07 15:18:32 +0800

    chore(lint): add estlint(airbnb base)

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: 'airbnb-base',
+
+  // add your custom rules here
+  'rules': {
+    'semi': [2, "never"],
+    'space-before-function-paren': [0],
+    'no-shadow': [0],
+    'no-param-reassign': [0],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
