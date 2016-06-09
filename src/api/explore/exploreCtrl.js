b2bb86e54e42ccf8a111a9385ad1d3ea29ae0d1c
Drake Leung <drakeleung42@gmail.com>
2016-06-10 02:18:19 +0800

    feat(api/explore): update 'getAllPhoto' & add 'likePhoto'

--- src/api/explore/exploreCtrl.js
+++ src/api/explore/exploreCtrl.js
@@ -8,10 +8,31 @@ const router = new Router()
 router
   .get('/', ctx =>
     exploreModel.getAllPhotos()
-      .then(result => {
+      .then(async (result) => {
+        const photoUserLikes = await exploreModel.getPhotoUserLikes()
+        const orderedPhotoUserLikes = photoUserLikes.reduce((prev, current) => {
+          const index = prev.findIndex(p => p.photo_id === current.photo_id)
+
+          if (index === -1) {
+            prev.push({
+              photo_id: current.photo_id,
+              user_id: [current.user_id],
+            })
+          } else {
+            prev[index].user_id.push(current.user_id)
+          }
+
+          return prev
+        }, [])
+
         const photos = result.map(p => {
+          const item = orderedPhotoUserLikes.find(o => o.photo_id === p.id)
+          const liked = item && ctx.session.user != null &&
+            item.user_id.indexOf(ctx.session.user.id) !== -1
           const { width, height } = sizeOf(`./public${p.url}`)
+
           return Object.assign(p, {
+            liked: !!liked,
             width,
             height,
             aspectRadio: width / height,
@@ -22,6 +43,20 @@ router
           success: true,
           data: photos,
         }
+      })
+  )
+
+  .post('/like/:photoId', ctx =>
+    exploreModel.likePhoto(ctx.params.photoId, ctx.session.user.id)
+      .then(result => {
+        ctx.body = { success: !!result }
+      })
+  )
+
+  .delete('/like/:photoId', ctx =>
+    exploreModel.dislikePhoto(ctx.params.photoId, ctx.session.user.id)
+      .then(result => {
+        ctx.body = { success: !!result }
       })
   )
 
--- src/api/explore/exploreModel.js
+++ src/api/explore/exploreModel.js
@@ -2,8 +2,43 @@ import connection from '../../config/db'
 
 export default {
   getAllPhotos () {
+    const query = `
+      SELECT
+        photo.id, photo.name, photo.description,
+        photo.url, photo.upload_user_id,
+        count(user_id) AS likes
+      FROM photo
+      LEFT JOIN photo_user_likes ON
+        photo.id = photo_user_likes.photo_id
+      GROUP BY photo.id
+    `
     return connection
-      .queryAsync('SELECT * FROM photo')
+      .queryAsync(query)
       .then(result => result)
+  },
+
+  async getPhotoUserLikes () {
+    const result = await connection
+      .queryAsync('SELECT * FROM photo_user_likes')
+
+    return result
+  },
+
+  likePhoto (photoId, userId) {
+    return connection
+      .queryAsync(
+        'INSERT INTO photo_user_likes SET ?',
+        { photo_id: photoId, user_id: userId }
+      )
+      .then(result => result.affectedRows)
+  },
+
+  dislikePhoto (photoId, userId) {
+    return connection
+      .queryAsync(
+        'DELETE FROM photo_user_likes WHERE photo_id = ? AND user_id = ?',
+        [photoId, userId]
+      )
+      .then(result => result.affectedRows)
   },
 }
